e4a31fd12b3af55c48c4813be5a60c3a4ea3d97e
xmanu <xmanu.linguet@gmail.com>
2021-02-06 17:13:51 +0100

    3

--- app/assets/js/serverstatus.js
+++ app/assets/js/serverstatus.js
@@ -8,7 +8,7 @@ const net = require('net')
  * @returns {Promise.<Object>} A promise which resolves to an object containing
  * status information.
  */
-exports.getStatus = function(address, port = 25565){
+exports.getStatus = function(address, port = 28056){
 
     if(port == null || port == ''){
         port = 25565
