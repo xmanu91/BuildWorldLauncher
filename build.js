971a33b5e7779b1ebb8466af456c1097f524d832
BuildTools <unconfigured@null.spigotmc.org>
2021-02-08 19:23:01 +0100

    4

--- build.js
+++ build.js
@@ -1,68 +1,73 @@
-const builder = require('electron-builder')
-const Platform = builder.Platform
+const builder = require("electron-builder");
+const Platform = builder.Platform;
 
-function getCurrentPlatform(){
-    switch(process.platform){
-        case 'win32':
-            return Platform.WINDOWS
-        case 'darwin':
-            return Platform.MAC
-        case 'linux':
-            return Platform.linux
-        default:
-            console.error('Cannot resolve current platform!')
-            return undefined
-    }
+function getCurrentPlatform() {
+  switch (process.platform) {
+    case "win32":
+      return Platform.WINDOWS;
+    case "darwin":
+      return Platform.MAC;
+    case "linux":
+      return Platform.linux;
+    default:
+      console.error("Cannot resolve current platform!");
+      return undefined;
+  }
 }
 
-builder.build({
-    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+builder
+  .build({
+    targets: (process.argv[2] != null && Platform[process.argv[2]] != null
+      ? Platform[process.argv[2]]
+      : getCurrentPlatform()
+    ).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
-        artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
-        directories: {
-            buildResources: 'build',
-            output: 'dist'
-        },
-        win: {
-            target: [
-                {
-                    target: 'nsis',
-                    arch: 'x64'
-                }
-            ]
-        },
-        nsis: {
-            oneClick: false,
-            perMachine: false,
-            allowElevation: true,
-            allowToChangeInstallationDirectory: true
-        },
-        mac: {
-            target: 'dmg',
-            category: 'public.app-category.games'
-        },
-        linux: {
-            target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
-            category: 'Game'
-        },
-        compression: 'maximum',
-        files: [
-            '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}'
+      appId: "buildworldlauncher",
+      productName: "BuildWorld Launcher",
+      artifactName: "${productName}-setup-${version}.${ext}",
+      copyright: "Copyright © 2018-2020 Daniel Scalzi and Xmanu91",
+      directories: {
+        buildResources: "build",
+        output: "dist",
+      },
+      win: {
+        target: [
+          {
+            target: "nsis",
+            arch: "x64",
+          },
         ],
-        extraResources: [
-            'libraries'
-        ],
-        asar: true
-    }
-}).then(() => {
-    console.log('Build complete!')
-}).catch(err => {
-    console.error('Error during build!', err)
-})
+      },
+      nsis: {
+        oneClick: false,
+        perMachine: false,
+        allowElevation: true,
+        allowToChangeInstallationDirectory: true,
+      },
+      mac: {
+        target: "dmg",
+        category: "public.app-category.games",
+      },
+      linux: {
+        target: "AppImage",
+        maintainer: "Daniel Scalzi",
+        vendor: "Daniel Scalzi",
+        synopsis: "Modded Minecraft Launcher",
+        description:
+          "Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.",
+        category: "Game",
+      },
+      compression: "maximum",
+      files: [
+        "!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}",
+      ],
+      extraResources: ["libraries"],
+      asar: true,
+    },
+  })
+  .then(() => {
+    console.log("Build complete!");
+  })
+  .catch((err) => {
+    console.error("Error during build!", err);
+  });
